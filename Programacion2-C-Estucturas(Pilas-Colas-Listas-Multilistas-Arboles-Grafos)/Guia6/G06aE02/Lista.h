2b57fcf948a9806c26f33dfbc190d54e6c7a7c4a
JuanMVicente <104178640+JuanMVicente@users.noreply.github.com>
2022-07-04 13:06:33 -0300

    Delete Programacion2-C-Estucturas(Pilas-Colas-Listas-Multilistas-Arboles-Grafos)/Guia6 directory

--- Programacion2-C-Estucturas(Pilas-Colas-Listas-Multilistas-Arboles-Grafos)/Guia6/G06aE02/Lista.h
+++
@@ -1,9 +0,0 @@
-typedef struct nodo{
-    int dato;
-    struct nodo * sig;} nodo;
-
-typedef nodo * TLista;
-
-void Genera_pi2(TLista *l, TLista *li, TLista *lp);
-void listaV(TLista * l, int x);
-void muestraL(TLista l);
